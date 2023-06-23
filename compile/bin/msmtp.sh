c6b8231203609f765681abe02ecda3d17ce106b9
Behm, John <john.behm@bertelsmann.de>
2023-06-23 17:47:31 +0200

    update msmtp & libgsasl compilation instructions

--- compile/bin/msmtp.sh
+++ compile/bin/msmtp.sh
@@ -12,15 +12,20 @@ init "$SCRIPT_PATH"
 export PKG_CONFIG_PATH="$PKG_CONFIG_PATH"
 export CFLAGS="$CFLAGS"
 export CPPFLAGS="$CPPFLAGS"
-export LDFLAGS="$LDFLAGS $LIBS $LIBS64"
+export LDFLAGS="$LDFLAGS"
 # -lrt -ldl -lidn -lntlm -lgss -lgsasl
 cd_src_dir
 files_before_build
 
+# undefined references:
+#   __res_query ns_initparse ns_parserr __dn_expand   -> -lresolv
+#   clock_gettime                                     -> -lrt
+#   dlfcn_load dlopen dlerror dlclose dlfcn_unload    -> -ldl
+
 
 autoreconf -i -I "$PREFIX/include" && \
 ./configure --prefix=$PREFIX --without-msmtpd --disable-nls --with-tls=openssl --with-libidn --with-libgsasl --with-libsecret=no &&
-make -j $CORES &&
+make V=1 -j $CORES LIBS="-lgsasl -lgss -lidn -lntlm -ldl -lrt -lresolv" &&
 make install &&
 RC=0 || RC=1
 
--- compile/lib/libgsasl.sh
+++ compile/lib/libgsasl.sh
@@ -15,13 +15,13 @@ export CPPFLAGS="$CPPFLAGS"
 # https://www.gnu.org/software/gsasl/
 export CFLAGS="-std=c99 -std=gnu99 $CFLAGS"
 export LDFLAGS="$LDFLAGS"
-#  -lidn -lntlm -lgss
+
 mk_build_dir
 cd_src_dir
 files_before_build
 
 ./configure --prefix=$PREFIX --enable-static --disable-shared && \
-make -j $CORES && \
+make -j $CORES LIBS="-lidn -lntlm -lgss" && \
 make check && \
 make install && \
 RC=0 || RC=1
