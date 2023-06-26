22d1ea1ab1a2f29f069caf775e052a659d8ff500
Behm, John <john.behm@bertelsmann.de>
2023-06-26 12:31:24 +0200

    update libgsasl linked libraries

--- compile/lib/libgsasl.sh
+++ compile/lib/libgsasl.sh
@@ -20,8 +20,9 @@ mk_build_dir
 cd_src_dir
 files_before_build
 
+# -l:libXXX.a forces to use the static library
 ./configure --prefix=$PREFIX --enable-static --disable-shared && \
-make -j $CORES LIBS="-lidn -lntlm -lgss" && \
+make -j $CORES LIBS="-l:libgss.a -l:libidn.a -l:libntlm.a" && \
 make check && \
 make install && \
 RC=0 || RC=1
